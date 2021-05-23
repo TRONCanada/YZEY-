839ac60d92ec5fe4d3a8dd4fb489c7a36d5f730f
@cpp-chaitanya <cpp-chaitanya@mobiloitte.com>
2021-05-23 17:13:37 +0530

    turtle test contracts added

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -1,10 +1,10 @@
 export default {
   cake: {
-    56: '0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
+    56: '0xe438e70ad3B04eb6dd1B0e5041B7c4173B682c03',
     97: '',
   },
   masterChef: {
-    56: '0xe70E9185F5ea7Ba3C5d63705784D8563017f2E57',
+    56: '0x14615dfc237F6620b02B96Eef51eEe3Afd468db9',
     97: '',
   },
   wbnb: {
