5ca34142ac813b7e8e60cc1fddf4853700a63e3b
aman_invincix <aman.jain@invincix.com>
2021-05-27 22:19:18 +0530

    Changed text and images

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -51,7 +51,7 @@ const config: MenuEntry[] = [
     items: [
       {
         label: 'PancakeSwap',
-        href: 'https://pancakeswap.info/token/0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
+        href: 'https://pancakeswap.info/token/0x673c63Ceed0EEE02c886E99fD60E46bC8D2C6A26',
       },
       {
         label: 'CoinGecko',
